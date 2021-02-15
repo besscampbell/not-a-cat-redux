43972839409ebfd3e36b6c8ae6bf5bfcc73c6ab3
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-15 10:33:21 -0800

    Show Add/update test is passing
    co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- src/__tests__/reducers/item-list-reducer.test.js
+++ src/__tests__/reducers/item-list-reducer.test.js
@@ -1,7 +1,36 @@
 import itemListReducer from '../../reducers/item-list-reducer';
 
 describe('itemListReducer', () => {
+
+  let action;
+  const itemData = {
+    name: 'Cat filter',
+    description: 'Changes face into cat face',
+    quantity: '10',
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(itemListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new item data to the masterItemList', () => {
+    const { name, description, quantity, id } = itemData;
+    action = {
+      type: 'ADD_OR_UPDATE_ITEM',
+      name: name,
+      description: description,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(itemListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        description: description,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/item-list-reducer.js
+++ src/reducers/item-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state = {}, action) => {
-  return state;
-};
+  const { type, name, description, quantity, id } = action;
+  switch(type) {
+    case 'ADD_OR_UPDATE_ITEM':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          quantity: quantity,
+          id: id
+        }
+      });
+    default:
+      return state;
+    }
+  };
